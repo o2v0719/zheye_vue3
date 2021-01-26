9f1a6b8cca3badf1459174399b486ed175e6d946
神经蛙 <470408436@qq.com>
2021-01-26 23:48:41 +0800

    finish optimization on cache

--- src/store.ts
+++ src/store.ts
@@ -53,8 +53,9 @@ export interface GlobalErrorProps {
 export interface GlobalDataProps {
   error: GlobalErrorProps;
   loading: boolean;
-  columns: ListProps<ColumnProps>;
-  posts: ListProps<PostProps>;
+  columns: { data: ListProps<ColumnProps>; isLoaded: boolean };
+  // loadedColumns 保存已经加载过的post-id
+  posts: { data: ListProps<PostProps>; loadedColumns: string[] };
   user: UserProps;
   token: string;
 }
@@ -85,10 +86,16 @@ const asyncAndCommit = async (
   url: string,
   mutationName: string,
   commit: Commit,
-  config: AxiosRequestConfig = { method: 'get' }
+  config: AxiosRequestConfig = { method: 'get' },
+  extraData?: any
 ) => {
   const { data } = await axios(url, config);
-  commit(mutationName, data);
+  if (extraData) {
+    commit(mutationName, { data, extraData });
+  } else {
+    commit(mutationName, data);
+  }
+
   // console.log(data);
   return data;
 };
@@ -97,8 +104,8 @@ const store = createStore<GlobalDataProps>({
     error: { status: false },
     token: localStorage.getItem('token') || '',
     loading: false,
-    columns: {},
-    posts: {},
+    columns: { data: {}, isLoaded: false },
+    posts: { data: {}, loadedColumns: [] },
     user: { isLogin: false }
   },
   mutations: {
@@ -112,16 +119,22 @@ const store = createStore<GlobalDataProps>({
       };
     }, */
     createPost(state, newPost) {
-      state.posts[newPost._id] = newPost;
+      state.posts.data[newPost._id] = newPost;
     },
     fetchColumns(state, rawData) {
-      state.columns = arrToObj(rawData.data.list);
+      state.columns.data = arrToObj(rawData.data.list);
+      state.columns.isLoaded = true;
     },
     fetchColumn(state, rawData) {
-      state.columns[rawData.data._id] = rawData.data;
+      state.columns.data[rawData.data._id] = rawData.data;
     },
-    fetchPosts(state, rawData) {
-      state.posts = arrToObj(rawData.data.list);
+    fetchPosts(state, { data: rawData, extraData: columnId }) {
+      // 如果请求完一个column，再请求另一个column 会把第一个column里面的data扔掉。所以需要合并原始对象。展开语法。
+      state.posts.data = {
+        ...state.posts.data,
+        ...arrToObj(rawData.data.list)
+      };
+      state.posts.loadedColumns.push(columnId);
     },
     setLoading(state, status) {
       state.loading = status;
@@ -149,13 +162,13 @@ const store = createStore<GlobalDataProps>({
       delete axios.defaults.headers.common.Authorization;
     },
     fetchPost(state, rawData) {
-      state.posts[rawData.data._id] = rawData.data;
+      state.posts.data[rawData.data._id] = rawData.data;
     },
     updatePost(state, { data }) {
-      state.posts[data._id] = data;
+      state.posts.data[data._id] = data;
     },
     deletePost(state, { data }) {
-      delete state.posts[data._id];
+      delete state.posts.data[data._id];
     }
   },
   actions: {
@@ -165,8 +178,10 @@ const store = createStore<GlobalDataProps>({
       ctx.commit('fetchColumns', data);
     }, */
     // 使用封装过的函数发起axios请求
-    fetchColumns({ commit }) {
-      return getAndCommit('/columns', 'fetchColumns', commit);
+    fetchColumns({ state, commit }) {
+      if (!state.columns.isLoaded) {
+        return asyncAndCommit('/columns', 'fetchColumns', commit);
+      }
     },
     // 使用参数解构来简化代码
     /* fetchColumn({ commit }, cid) {
@@ -182,11 +197,23 @@ const store = createStore<GlobalDataProps>({
       const { data } = await axios.get(`/columns/${cid}/posts`);
       commit('fetchPosts', data);
     } */
-    fetchColumn({ commit }, cid) {
-      return getAndCommit(`/columns/${cid}`, 'fetchColumn', commit);
+    fetchColumn({ state, commit }, cid) {
+      if (!state.columns.data[cid]) {
+        return asyncAndCommit(`/columns/${cid}`, 'fetchColumn', commit);
+      }
     },
-    fetchPosts({ commit }, cid) {
-      return getAndCommit(`/columns/${cid}/posts`, 'fetchPosts', commit);
+    fetchPosts({ state, commit }, cid) {
+      if (!state.posts.loadedColumns.includes(cid)) {
+        return asyncAndCommit(
+          `/columns/${cid}/posts`,
+          'fetchPosts',
+          commit,
+          {
+            method: 'get'
+          },
+          cid
+        );
+      }
     },
     login({ commit }, payload) {
       return postAndCommit('/user/login', 'login', commit, payload);
@@ -208,8 +235,16 @@ const store = createStore<GlobalDataProps>({
       });
     },
     // 请求指定id的文章
-    fetchPost({ commit }, pid) {
-      return asyncAndCommit(`/posts/${pid}`, 'fetchPost', commit);
+    fetchPost({ state, commit }, pid) {
+      // 由于fetchPosts 也会改变state.posts.data, 但是fetchPosts和fetchPost改变的state.posts.data 里面的数据是不一样的。fetchPost返回的data带有content。
+      const currentPost = state.posts.data[pid];
+      if (!currentPost || !currentPost.content) {
+        return asyncAndCommit(`/posts/${pid}`, 'fetchPost', commit);
+      } else {
+        // 如果从缓存里获取，则没有Promise对象
+        // createPost.vue  里面需要dispatch后面的then方法处理promise对象
+        return Promise.resolve({ data: currentPost });
+      }
     },
     // 编辑后更新文章
     updatePost({ commit }, { id, payload }) {
@@ -227,18 +262,18 @@ const store = createStore<GlobalDataProps>({
   },
   getters: {
     getColumns: state => {
-      return objToArr(state.columns);
+      return objToArr(state.columns.data);
     },
     getColumnById(state) {
       return (id: string) => {
-        return state.columns[id];
+        return state.columns.data[id];
       };
     },
     getPostsByCid: state => (cid: string) => {
-      return objToArr(state.posts).filter(post => post.column === cid);
+      return objToArr(state.posts.data).filter(post => post.column === cid);
     },
     getCurrentPost: state => (pid: string) => {
-      return state.posts[pid];
+      return state.posts.data[pid];
     }
   }
 });
